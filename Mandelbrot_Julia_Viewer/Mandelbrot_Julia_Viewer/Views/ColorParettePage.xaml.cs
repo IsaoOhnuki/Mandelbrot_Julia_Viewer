7d085a2ac3e44d75b2533b8415d07c07ba3b469f
KAIKAN-6\kaikan-6 <isa0.net@ezweb.ne.jp>
2017-08-31 14:49:43 +0900

    パレットページ調整

--- Mandelbrot_Julia_Viewer/Mandelbrot_Julia_Viewer/Views/ColorParettePage.xaml.cs
+++ Mandelbrot_Julia_Viewer/Mandelbrot_Julia_Viewer/Views/ColorParettePage.xaml.cs
@@ -3,6 +3,7 @@ using Mandelbrot_Julia_Viewer.ViewModels;
 using Models;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,51 +18,68 @@ namespace Mandelbrot_Julia_Viewer.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ColorParettePage : ContentPage
     {
-        private Mandelbrot_Julia.ColorResolutionStruct[] colorParette;
+        public ObservableCollection<Mandelbrot_Julia.ColorResolutionStruct> Parette { get; set; }
         public Mandelbrot_Julia.ColorResolutionStruct[] ColorParette
         {
-            get { return colorParette; }
+            get { return Parette.ToArray(); }
             set
             {
-                colorParette = null;
-                if (value != null)
-                {
-                    colorParette = value;
-                    //colorParette = new Mandelbrot_Julia.ColorResolutionStruct[value.GetLength(0)];
-                    //value.CopyTo(colorParette, 0);
+                Parette.Clear();
+                foreach (var val in value)
+                    Parette.Add(val);
+            }
+        }
 
-                    foreach (var parette in ColorParette)
-                    {
-                        var layout = new Grid();
-                        layout.BindingContext = parette;
-                        layout.ColumnDefinitions = new ColumnDefinitionCollection();
-                        layout.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(6, GridUnitType.Star) });
-                        layout.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
-                        layout.BackgroundColor = Color.DarkKhaki;
-                        layout.HorizontalOptions = LayoutOptions.FillAndExpand;
-                        var slider = new Slider();
-                        slider.SetBinding(Slider.ValueProperty, new Binding("Posision", BindingMode.TwoWay));
-                        slider.HorizontalOptions = LayoutOptions.FillAndExpand;
-                        slider.SetValue(Grid.ColumnProperty, 0);
-                        layout.Children.Add(slider);
-                        var colsel = new ColorPicker();
-                        colsel.SetBinding(ColorPicker.SelectedColorProperty, new Binding("Color", BindingMode.TwoWay));
-                        colsel.SelectedColor = parette.Color;
-                        colsel.SetValue(Grid.ColumnProperty, 1);
-                        layout.Children.Add(colsel);
-                        StackLayout.Children.Add(layout);
-                    }
+        private void Parette_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        {
+            if (e.Action == System.Collections.Specialized.NotifyCollectionChangedAction.Add)
+            {
+                int insertIndex = e.NewStartingIndex;
+                foreach (var parette in e.NewItems)
+                {
+                    var layout = new Grid();
+                    layout.BindingContext = parette;
+                    layout.ColumnDefinitions = new ColumnDefinitionCollection();
+                    layout.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(6, GridUnitType.Star) });
+                    layout.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+                    layout.BackgroundColor = Color.DarkKhaki;
+                    layout.HorizontalOptions = LayoutOptions.FillAndExpand;
+                    var slider = new Slider();
+                    slider.SetBinding(Slider.ValueProperty, new Binding("Posision", BindingMode.TwoWay));
+                    slider.HorizontalOptions = LayoutOptions.FillAndExpand;
+                    slider.SetValue(Grid.ColumnProperty, 0);
+                    layout.Children.Add(slider);
+                    var colsel = new ColorPicker();
+                    colsel.SetBinding(ColorPicker.SelectedColorProperty, new Binding("Color", BindingMode.TwoWay));
+                    colsel.SetValue(Grid.ColumnProperty, 1);
+                    layout.Children.Add(colsel);
+                    StackLayout.Children.Insert(insertIndex ++, layout);
+                }
+            }
+            if (e.Action == System.Collections.Specialized.NotifyCollectionChangedAction.Remove)
+            {
+                for (int l = 0; l < e.OldItems.Count; ++l)
+                {
+                    StackLayout.Children.RemoveAt(e.OldStartingIndex + l);
                 }
             }
         }
 
         public ColorParettePage()
         {
+            Parette = new ObservableCollection<Mandelbrot_Julia.ColorResolutionStruct>();
+            Parette.CollectionChanged += Parette_CollectionChanged;
+
             InitializeComponent();
+
             ToolbarItems.Add(new ToolbarItem("<", "", () => { }, ToolbarItemOrder.Default));
         }
+
         public ColorParettePage(MJViewModel viewModel)
         {
+            Parette = new ObservableCollection<Mandelbrot_Julia.ColorResolutionStruct>();
+            Parette.CollectionChanged += Parette_CollectionChanged;
+
             InitializeComponent();
 
             ColorParette = viewModel.ColorParette;
