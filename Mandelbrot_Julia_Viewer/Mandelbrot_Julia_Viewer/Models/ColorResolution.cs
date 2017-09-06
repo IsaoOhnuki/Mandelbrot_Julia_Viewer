d1f4a50314eb8d8081c1a316bfc3f2245fc53a6f
ISAO-SERVER\isao <isa0.net@ezweb.ne.jp>
2017-09-06 23:23:06 +0900

    ColorResolutionクラス分離

---
+++ Mandelbrot_Julia_Viewer/Mandelbrot_Julia_Viewer/Models/ColorResolution.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Models
+{
+    public class ColorResolution
+    {
+        public ColorResolutionStruct[] ColorResolutions { get; set; }
+        public Color[] Colors { get; set; }
+        public ColorResolution(ColorResolution parette)
+        {
+            if (parette as object == null || parette.Colors == null)
+                return;
+            Colors = new Color[parette.Colors.GetLength(0)];
+            parette.Colors.CopyTo(Colors, 0);
+            ColorResolutions = new ColorResolutionStruct[parette.ColorResolutions.GetLength(0)];
+            parette.ColorResolutions.CopyTo(ColorResolutions, 0);
+        }
+        public ColorResolution(int repert, ColorResolutionStruct[] colorResolutions)
+        {
+            if (colorResolutions as object == null)
+                return;
+            Colors = Parette(repert, colorResolutions);
+            ColorResolutions = new ColorResolutionStruct[colorResolutions.GetLength(0)];
+            colorResolutions.CopyTo(ColorResolutions, 0);
+        }
+        public static implicit operator Color[] (ColorResolution parette)
+        {
+            if (parette as object == null || parette.Colors == null)
+                return null;
+            Color[] ret = new Color[parette.Colors.GetLength(0)];
+            parette.Colors.CopyTo(ret, 0);
+            return ret;
+        }
+        // http://www.sofgate.com/design/ct_gradation.html グラデーション配色の計算方法
+        public static Color[] Parette(int repert, ColorResolutionStruct[] cols)
+        {
+            Color[] ret = new Color[repert + 1];
+            for (int arrayidx = 0; arrayidx < repert; ++arrayidx)
+            {
+                double pos = (double)arrayidx / (repert - 1);
+                int prevcolpos = 0;
+                for (int colpos = 1; colpos < cols.Count(); ++colpos)
+                {
+                    if (pos > cols[prevcolpos].Position && pos <= cols[colpos].Position)
+                    {
+                        double r = (cols[colpos].Color.R - cols[prevcolpos].Color.R) * pos / (cols[colpos].Position - cols[prevcolpos].Position) + cols[prevcolpos].Color.R;
+                        double g = (cols[colpos].Color.G - cols[prevcolpos].Color.G) * pos / (cols[colpos].Position - cols[prevcolpos].Position) + cols[prevcolpos].Color.G;
+                        double b = (cols[colpos].Color.B - cols[prevcolpos].Color.B) * pos / (cols[colpos].Position - cols[prevcolpos].Position) + cols[prevcolpos].Color.B;
+                        ret[arrayidx] = new Color(r, g, b);
+                        break;
+                    }
+                }
+            }
+            ret[repert] = ret[0];
+            return ret;
+        }
+        public static bool operator ==(ColorResolution l, ColorResolution r)
+        {
+            if (l as object == null || r as object == null || l.Colors == null || r.Colors == null)
+                return false;
+            bool ret = l.Colors.GetLength(0) == r.Colors.GetLength(0);
+            for (int idx = 0; ret && idx < l.Colors.GetLength(0); ++idx)
+            {
+                ret = l.Colors[idx] == r.Colors[idx];
+            }
+            return ret;
+        }
+        public static bool operator !=(ColorResolution l, ColorResolution r)
+        {
+            if (l as object == null || r as object == null || l.Colors == null || r.Colors == null)
+                return true;
+            bool ret = l.Colors.GetLength(0) != r.Colors.GetLength(0);
+            for (int idx = 0; !ret && idx < l.Colors.GetLength(0); ++idx)
+            {
+                ret = l.Colors[idx] != r.Colors[idx];
+            }
+            return ret;
+        }
+        public override bool Equals(object obj)
+        {
+            return this == obj as ColorResolution;
+        }
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+    }
+
+    public struct ColorResolutionStruct
+    {
+        public Color Color { get; set; }
+        public double Position { get; set; }
+    }
+}
--- Mandelbrot_Julia_Viewer/Mandelbrot_Julia_Viewer/Models/MJClass.cs
+++ Mandelbrot_Julia_Viewer/Mandelbrot_Julia_Viewer/Models/MJClass.cs
@@ -17,9 +17,9 @@ namespace Models
         public double Radius { get; set; }
         public int Repert { get; set; }
         public int Resolution { get; set; }
-        public int ParetteType { get; set; }
+        //public int ParetteType { get; set; }
         public int JuliaMapSplit { get; set; }
-        public Parette ColorParette { get; set; }
+        //public Parette ColorParette { get; set; }
         private int[] data;
         public int[] Data
         {
@@ -57,23 +57,23 @@ namespace Models
             Radius = mj.Radius;
             Repert = mj.Repert;
             Resolution = mj.Resolution;
-            ParetteType = mj.ParetteType;
-            ColorParette = mj.ColorParette;
+            //ParetteType = mj.ParetteType;
+            //ColorParette = mj.ColorParette;
             Data = mj.Data;
             Image = mj.Image;
         }
-        public Mandelbrot_Julia(double xpos, double ypos, int repert, int resolution, int split, int colorParette, ColorResolutionStruct[] colors)
+        public Mandelbrot_Julia(double xpos, double ypos, double radius, int repert, int resolution, int split, int colorParette, ColorResolutionStruct[] colors)
         {
             IPos = double.NaN;
             JPos = double.NaN;
             XPos = xpos;
             YPos = ypos;
-            Radius = 0;
+            Radius = radius;
             Repert = repert;
             Resolution = resolution;
             JuliaMapSplit = split;
-            ParetteType = colorParette;
-            ColorParette = new Parette(repert, colors);
+            //ParetteType = colorParette;
+            //ColorParette = new Parette(repert, colors);
         }
         public Mandelbrot_Julia(double xpos, double ypos, double radius, int repert, int resolution, int colorParette, ColorResolutionStruct[] colors)
         {
@@ -85,8 +85,8 @@ namespace Models
             Repert = repert;
             Resolution = resolution;
             JuliaMapSplit = 0;
-            ParetteType = colorParette;
-            ColorParette = new Parette(repert, colors);
+            //ParetteType = colorParette;
+            //ColorParette = new Parette(repert, colors);
         }
         public Mandelbrot_Julia(double ipos, double jpos, double xpos, double ypos, double radius, int repert, int resolution, int colorParette, ColorResolutionStruct[] colors)
         {
@@ -98,8 +98,8 @@ namespace Models
             Repert = repert;
             Resolution = resolution;
             JuliaMapSplit = 0;
-            ParetteType = colorParette;
-            ColorParette = new Parette(repert, colors);
+            //ParetteType = colorParette;
+            //ColorParette = new Parette(repert, colors);
         }
 
         public static bool operator ==(Mandelbrot_Julia l, Mandelbrot_Julia r)
@@ -122,8 +122,8 @@ namespace Models
                 && Repert == ((Mandelbrot_Julia)obj).Repert
                 && Resolution == ((Mandelbrot_Julia)obj).Resolution
                 && JuliaMapSplit == ((Mandelbrot_Julia)obj).JuliaMapSplit
-                && ParetteType == ((Mandelbrot_Julia)obj).ParetteType
-                && ColorParette == ((Mandelbrot_Julia)obj).ColorParette;
+                //&& ParetteType == ((Mandelbrot_Julia)obj).ParetteType
+                /*&& ColorParette == ((Mandelbrot_Julia)obj).ColorParette*/;
         }
 
         public override int GetHashCode()
@@ -162,95 +162,95 @@ namespace Models
             });
         }
 
-        public class ColorResolutionStruct
-        {
-            public Color Color { get; set; }
-            public double Position { get; set; }
-        }
+        //public class ColorResolutionStruct
+        //{
+        //    public Color Color { get; set; }
+        //    public double Position { get; set; }
+        //}
 
-        public class Parette
-        {
-            public ColorResolutionStruct[] ColorResolutions { get; set; }
-            public Color[] Colors { get; set; }
-            public Parette(Parette parette)
-            {
-                if (parette as object == null || parette.Colors == null)
-                    return;
-                Colors = new Color[parette.Colors.GetLength(0)];
-                parette.Colors.CopyTo(Colors, 0);
-                ColorResolutions = new ColorResolutionStruct[parette.ColorResolutions.GetLength(0)];
-                parette.ColorResolutions.CopyTo(ColorResolutions, 0);
-            }
-            public Parette(int repert, ColorResolutionStruct[] colorResolutions)
-            {
-                if (colorResolutions as object == null)
-                    return;
-                Colors = ColorResolution(repert, colorResolutions);
-                ColorResolutions = new ColorResolutionStruct[colorResolutions.GetLength(0)];
-                colorResolutions.CopyTo(ColorResolutions, 0);
-            }
-            public static implicit operator Color[](Parette parette)
-            {
-                if (parette as object == null || parette.Colors == null)
-                    return null;
-                Color[] ret = new Color[parette.Colors.GetLength(0)];
-                parette.Colors.CopyTo(ret, 0);
-                return ret;
-            }
-            // http://www.sofgate.com/design/ct_gradation.html グラデーション配色の計算方法
-            public static Color[] ColorResolution(int repert, ColorResolutionStruct[] cols)
-            {
-                Color[] ret = new Color[repert + 1];
-                for (int arrayidx = 0; arrayidx < repert; ++arrayidx)
-                {
-                    double pos = (double)arrayidx / (repert - 1);
-                    int prevcolpos = 0;
-                    for (int colpos = 1; colpos < cols.Count(); ++colpos)
-                    {
-                        if (pos > cols[prevcolpos].Position && pos <= cols[colpos].Position)
-                        {
-                            double r = (cols[colpos].Color.R - cols[prevcolpos].Color.R) * pos / (cols[colpos].Position - cols[prevcolpos].Position) + cols[prevcolpos].Color.R;
-                            double g = (cols[colpos].Color.G - cols[prevcolpos].Color.G) * pos / (cols[colpos].Position - cols[prevcolpos].Position) + cols[prevcolpos].Color.G;
-                            double b = (cols[colpos].Color.B - cols[prevcolpos].Color.B) * pos / (cols[colpos].Position - cols[prevcolpos].Position) + cols[prevcolpos].Color.B;
-                            ret[arrayidx] = new Color(r, g, b);
-                            break;
-                        }
-                    }
-                }
-                ret[repert] = ret[0];
-                return ret;
-            }
-            public static bool operator ==(Parette l, Parette r)
-            {
-                if (l as object == null || r as object == null || l.Colors == null || r.Colors == null)
-                    return false;
-                bool ret = l.Colors.GetLength(0) == r.Colors.GetLength(0);
-                for (int idx = 0; ret && idx < l.Colors.GetLength(0); ++idx)
-                {
-                    ret = l.Colors[idx] == r.Colors[idx];
-                }
-                return ret;
-            }
-            public static bool operator !=(Parette l, Parette r)
-            {
-                if (l as object == null || r as object == null || l.Colors == null || r.Colors == null)
-                    return true;
-                bool ret = l.Colors.GetLength(0) != r.Colors.GetLength(0);
-                for (int idx = 0; !ret && idx < l.Colors.GetLength(0); ++idx)
-                {
-                    ret = l.Colors[idx] != r.Colors[idx];
-                }
-                return ret;
-            }
-            public override bool Equals(object obj)
-            {
-                return this == obj as Parette;
-            }
-            public override int GetHashCode()
-            {
-                return base.GetHashCode();
-            }
-        }
+        //public class Parette
+        //{
+        //    public ColorResolutionStruct[] ColorResolutions { get; set; }
+        //    public Color[] Colors { get; set; }
+        //    public Parette(Parette parette)
+        //    {
+        //        if (parette as object == null || parette.Colors == null)
+        //            return;
+        //        Colors = new Color[parette.Colors.GetLength(0)];
+        //        parette.Colors.CopyTo(Colors, 0);
+        //        ColorResolutions = new ColorResolutionStruct[parette.ColorResolutions.GetLength(0)];
+        //        parette.ColorResolutions.CopyTo(ColorResolutions, 0);
+        //    }
+        //    public Parette(int repert, ColorResolutionStruct[] colorResolutions)
+        //    {
+        //        if (colorResolutions as object == null)
+        //            return;
+        //        Colors = ColorResolution(repert, colorResolutions);
+        //        ColorResolutions = new ColorResolutionStruct[colorResolutions.GetLength(0)];
+        //        colorResolutions.CopyTo(ColorResolutions, 0);
+        //    }
+        //    public static implicit operator Color[](Parette parette)
+        //    {
+        //        if (parette as object == null || parette.Colors == null)
+        //            return null;
+        //        Color[] ret = new Color[parette.Colors.GetLength(0)];
+        //        parette.Colors.CopyTo(ret, 0);
+        //        return ret;
+        //    }
+        //    // http://www.sofgate.com/design/ct_gradation.html グラデーション配色の計算方法
+        //    public static Color[] ColorResolution(int repert, ColorResolutionStruct[] cols)
+        //    {
+        //        Color[] ret = new Color[repert + 1];
+        //        for (int arrayidx = 0; arrayidx < repert; ++arrayidx)
+        //        {
+        //            double pos = (double)arrayidx / (repert - 1);
+        //            int prevcolpos = 0;
+        //            for (int colpos = 1; colpos < cols.Count(); ++colpos)
+        //            {
+        //                if (pos > cols[prevcolpos].Position && pos <= cols[colpos].Position)
+        //                {
+        //                    double r = (cols[colpos].Color.R - cols[prevcolpos].Color.R) * pos / (cols[colpos].Position - cols[prevcolpos].Position) + cols[prevcolpos].Color.R;
+        //                    double g = (cols[colpos].Color.G - cols[prevcolpos].Color.G) * pos / (cols[colpos].Position - cols[prevcolpos].Position) + cols[prevcolpos].Color.G;
+        //                    double b = (cols[colpos].Color.B - cols[prevcolpos].Color.B) * pos / (cols[colpos].Position - cols[prevcolpos].Position) + cols[prevcolpos].Color.B;
+        //                    ret[arrayidx] = new Color(r, g, b);
+        //                    break;
+        //                }
+        //            }
+        //        }
+        //        ret[repert] = ret[0];
+        //        return ret;
+        //    }
+        //    public static bool operator ==(Parette l, Parette r)
+        //    {
+        //        if (l as object == null || r as object == null || l.Colors == null || r.Colors == null)
+        //            return false;
+        //        bool ret = l.Colors.GetLength(0) == r.Colors.GetLength(0);
+        //        for (int idx = 0; ret && idx < l.Colors.GetLength(0); ++idx)
+        //        {
+        //            ret = l.Colors[idx] == r.Colors[idx];
+        //        }
+        //        return ret;
+        //    }
+        //    public static bool operator !=(Parette l, Parette r)
+        //    {
+        //        if (l as object == null || r as object == null || l.Colors == null || r.Colors == null)
+        //            return true;
+        //        bool ret = l.Colors.GetLength(0) != r.Colors.GetLength(0);
+        //        for (int idx = 0; !ret && idx < l.Colors.GetLength(0); ++idx)
+        //        {
+        //            ret = l.Colors[idx] != r.Colors[idx];
+        //        }
+        //        return ret;
+        //    }
+        //    public override bool Equals(object obj)
+        //    {
+        //        return this == obj as Parette;
+        //    }
+        //    public override int GetHashCode()
+        //    {
+        //        return base.GetHashCode();
+        //    }
+        //}
 
         //private class Complex
         //{
@@ -425,7 +425,7 @@ namespace Models
         //http://outside6.wp.xdomain.jp/2016/08/09/post-568/ TaskとawaitのデッドロックをTaskで回避する
         //http://qiita.com/acple@github/items/8f63aacb13de9954c5da Taskを極めろ！async/await完全攻略
 
-        public static Task<int[]> JuliaMap(double xpos, double ypos, int repert, int resolution, int split)
+        public static Task<int[]> JuliaMap(double xpos, double ypos, double radius, int repert, int resolution, int split)
         {
             return Task.Run<int[]>(() => {
                 int size = resolution / split;
@@ -433,7 +433,7 @@ namespace Models
 
                 int[] data = new int[resolution * resolution];
 
-                double radius = 2;
+                //double radius = 2;
                 double julia = radius * 2 / (split - 1);
 
                 List<Task<int[]>> tasks = new List<Task<int[]>>();
