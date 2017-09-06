df80d440738f8835999b27e56179f24d747e5557
ISAO-SERVER\isao <isa0.net@ezweb.ne.jp>
2017-09-07 04:45:29 +0900

    MJClass修正

--- Mandelbrot_Julia_Viewer/Mandelbrot_Julia_Viewer/Models/MJClass.cs
+++ Mandelbrot_Julia_Viewer/Mandelbrot_Julia_Viewer/Models/MJClass.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿#define ComplexClassDefine
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -17,9 +19,7 @@ namespace Models
         public double Radius { get; set; }
         public int Repert { get; set; }
         public int Resolution { get; set; }
-        //public int ParetteType { get; set; }
         public int JuliaMapSplit { get; set; }
-        //public Parette ColorParette { get; set; }
         private int[] data;
         public int[] Data
         {
@@ -57,8 +57,6 @@ namespace Models
             Radius = mj.Radius;
             Repert = mj.Repert;
             Resolution = mj.Resolution;
-            //ParetteType = mj.ParetteType;
-            //ColorParette = mj.ColorParette;
             Data = mj.Data;
             Image = mj.Image;
         }
@@ -72,8 +70,6 @@ namespace Models
             Repert = repert;
             Resolution = resolution;
             JuliaMapSplit = split;
-            //ParetteType = colorParette;
-            //ColorParette = new Parette(repert, colors);
         }
         public Mandelbrot_Julia(double xpos, double ypos, double radius, int repert, int resolution, /*int colorParette, */ColorResolutionStruct[] colors)
         {
@@ -85,8 +81,6 @@ namespace Models
             Repert = repert;
             Resolution = resolution;
             JuliaMapSplit = 0;
-            //ParetteType = colorParette;
-            //ColorParette = new Parette(repert, colors);
         }
         public Mandelbrot_Julia(double ipos, double jpos, double xpos, double ypos, double radius, int repert, int resolution, /*int colorParette, */ColorResolutionStruct[] colors)
         {
@@ -98,8 +92,6 @@ namespace Models
             Repert = repert;
             Resolution = resolution;
             JuliaMapSplit = 0;
-            //ParetteType = colorParette;
-            //ColorParette = new Parette(repert, colors);
         }
 
         public static bool operator ==(Mandelbrot_Julia l, Mandelbrot_Julia r)
@@ -121,9 +113,7 @@ namespace Models
                 && (Double.IsNaN(Radius) ? Double.IsNaN(((Mandelbrot_Julia)obj).Radius) : Radius == ((Mandelbrot_Julia)obj).Radius)
                 && Repert == ((Mandelbrot_Julia)obj).Repert
                 && Resolution == ((Mandelbrot_Julia)obj).Resolution
-                && JuliaMapSplit == ((Mandelbrot_Julia)obj).JuliaMapSplit
-                //&& ParetteType == ((Mandelbrot_Julia)obj).ParetteType
-                /*&& ColorParette == ((Mandelbrot_Julia)obj).ColorParette*/;
+                && JuliaMapSplit == ((Mandelbrot_Julia)obj).JuliaMapSplit;
         }
 
         public override int GetHashCode()
@@ -162,161 +152,74 @@ namespace Models
             });
         }
 
-        //public class ColorResolutionStruct
-        //{
-        //    public Color Color { get; set; }
-        //    public double Position { get; set; }
-        //}
+        private class Complex
+        {
+            public double Real { get; set; }
+            public double Imaginary { get; set; }
+            public double Magnitude { get { return Math.Sqrt(Real * Real + Imaginary * Imaginary); } }
+            public double Phase { get { return Math.Atan2(Imaginary, Real); } }
 
-        //public class Parette
-        //{
-        //    public ColorResolutionStruct[] ColorResolutions { get; set; }
-        //    public Color[] Colors { get; set; }
-        //    public Parette(Parette parette)
-        //    {
-        //        if (parette as object == null || parette.Colors == null)
-        //            return;
-        //        Colors = new Color[parette.Colors.GetLength(0)];
-        //        parette.Colors.CopyTo(Colors, 0);
-        //        ColorResolutions = new ColorResolutionStruct[parette.ColorResolutions.GetLength(0)];
-        //        parette.ColorResolutions.CopyTo(ColorResolutions, 0);
-        //    }
-        //    public Parette(int repert, ColorResolutionStruct[] colorResolutions)
-        //    {
-        //        if (colorResolutions as object == null)
-        //            return;
-        //        Colors = ColorResolution(repert, colorResolutions);
-        //        ColorResolutions = new ColorResolutionStruct[colorResolutions.GetLength(0)];
-        //        colorResolutions.CopyTo(ColorResolutions, 0);
-        //    }
-        //    public static implicit operator Color[](Parette parette)
-        //    {
-        //        if (parette as object == null || parette.Colors == null)
-        //            return null;
-        //        Color[] ret = new Color[parette.Colors.GetLength(0)];
-        //        parette.Colors.CopyTo(ret, 0);
-        //        return ret;
-        //    }
-        //    // http://www.sofgate.com/design/ct_gradation.html グラデーション配色の計算方法
-        //    public static Color[] ColorResolution(int repert, ColorResolutionStruct[] cols)
-        //    {
-        //        Color[] ret = new Color[repert + 1];
-        //        for (int arrayidx = 0; arrayidx < repert; ++arrayidx)
-        //        {
-        //            double pos = (double)arrayidx / (repert - 1);
-        //            int prevcolpos = 0;
-        //            for (int colpos = 1; colpos < cols.Count(); ++colpos)
-        //            {
-        //                if (pos > cols[prevcolpos].Position && pos <= cols[colpos].Position)
-        //                {
-        //                    double r = (cols[colpos].Color.R - cols[prevcolpos].Color.R) * pos / (cols[colpos].Position - cols[prevcolpos].Position) + cols[prevcolpos].Color.R;
-        //                    double g = (cols[colpos].Color.G - cols[prevcolpos].Color.G) * pos / (cols[colpos].Position - cols[prevcolpos].Position) + cols[prevcolpos].Color.G;
-        //                    double b = (cols[colpos].Color.B - cols[prevcolpos].Color.B) * pos / (cols[colpos].Position - cols[prevcolpos].Position) + cols[prevcolpos].Color.B;
-        //                    ret[arrayidx] = new Color(r, g, b);
-        //                    break;
-        //                }
-        //            }
-        //        }
-        //        ret[repert] = ret[0];
-        //        return ret;
-        //    }
-        //    public static bool operator ==(Parette l, Parette r)
-        //    {
-        //        if (l as object == null || r as object == null || l.Colors == null || r.Colors == null)
-        //            return false;
-        //        bool ret = l.Colors.GetLength(0) == r.Colors.GetLength(0);
-        //        for (int idx = 0; ret && idx < l.Colors.GetLength(0); ++idx)
-        //        {
-        //            ret = l.Colors[idx] == r.Colors[idx];
-        //        }
-        //        return ret;
-        //    }
-        //    public static bool operator !=(Parette l, Parette r)
-        //    {
-        //        if (l as object == null || r as object == null || l.Colors == null || r.Colors == null)
-        //            return true;
-        //        bool ret = l.Colors.GetLength(0) != r.Colors.GetLength(0);
-        //        for (int idx = 0; !ret && idx < l.Colors.GetLength(0); ++idx)
-        //        {
-        //            ret = l.Colors[idx] != r.Colors[idx];
-        //        }
-        //        return ret;
-        //    }
-        //    public override bool Equals(object obj)
-        //    {
-        //        return this == obj as Parette;
-        //    }
-        //    public override int GetHashCode()
-        //    {
-        //        return base.GetHashCode();
-        //    }
-        //}
+            public Complex(double real, double imaginary)
+            {
+                Real = real;
+                Imaginary = imaginary;
+            }
 
-        //private class Complex
-        //{
-        //    public double Real { get; set; }
-        //    public double Imaginary { get; set; }
-        //    public double Magnitude { get { return Math.Sqrt(Real * Real + Imaginary * Imaginary); } }
-        //    public double Phase { get { return Math.Atan2(Imaginary, Real); } }
+            public static Complex operator +(Complex l, Complex r)
+            {
+                return new Complex(l.Real + r.Real, l.Imaginary + r.Imaginary);
+            }
 
-        //    public Complex(double real, double imaginary)
-        //    {
-        //        Real = real;
-        //        Imaginary = imaginary;
-        //    }
-
-        //    public static Complex operator +(Complex l, Complex r)
-        //    {
-        //        return new Complex(l.Real + r.Real, l.Imaginary + r.Imaginary);
-        //    }
-
-        //    public static Complex operator *(Complex l, Complex r)
-        //    {
-        //        return new Complex(l.Real * r.Real - l.Imaginary * r.Imaginary, l.Real * r.Imaginary + r.Real * l.Imaginary);
-        //    }
-        //}
+            public static Complex operator *(Complex l, Complex r)
+            {
+                return new Complex(l.Real * r.Real - l.Imaginary * r.Imaginary, l.Real * r.Imaginary + r.Real * l.Imaginary);
+            }
+        }
 
         public static Task<int[]> Mandelbrot(double xpos, double ypos, double radius, int repert, int resolution)
         {
             return Task.Run<int[]>(() => {
                 int[] data = new int[resolution * resolution];
 
+#if ComplexClassDefine
                 #region Complex Code
-                //Complex minimum = new Complex(xpos - radius, ypos - radius);
-                //Complex maximum = new Complex(xpos + radius, ypos + radius);
+                Complex minimum = new Complex(xpos - radius, ypos - radius);
+                Complex maximum = new Complex(xpos + radius, ypos + radius);
                 #endregion
-
+#else
                 #region No Complex Code
                 double xmin = xpos - radius;
                 double ymin = ypos + radius;
                 #endregion
-
+#endif
                 int i = 0;
                 for (; i < resolution; i++)
                 {
+#if ComplexClassDefine
                     #region Complex Code
-                    //double x = i * (maximum.Real - minimum.Real) / (resolution - 1);
+                    double x = i * (maximum.Real - minimum.Real) / (resolution - 1);
                     #endregion
-
+#else
                     #region No Complex Code
                     double x = i * radius * 2 / (resolution - 1) + xmin;
                     #endregion
-
+#endif
                     for (int j = 0; j < resolution; j++)
                     {
                         int count = repert;
 
+#if ComplexClassDefine
                         #region Complex Code
-                        //double y = j * (maximum.Imaginary - minimum.Imaginary) / (resolution - 1);
-                        //Complex c = new Complex(minimum.Real + x, maximum.Imaginary - y);
-                        //Complex z = new Complex(c.Real, c.Imaginary);
-                        //while (z.Magnitude < 2 && count > 0)
-                        //{
-                        //    z = (z * z) + c;
-                        //    --count;
-                        //}
+                        double y = j * (maximum.Imaginary - minimum.Imaginary) / (resolution - 1);
+                        Complex c = new Complex(minimum.Real + x, maximum.Imaginary - y);
+                        Complex z = new Complex(c.Real, c.Imaginary);
+                        while (z.Magnitude < 2 && count > 0)
+                        {
+                            z = (z * z) + c;
+                            --count;
+                        }
                         #endregion
-
+#else
                         #region No Complex Code
                         double y = j * radius * 2 / (resolution - 1) - ymin;
                         double a = x;
@@ -332,7 +235,7 @@ namespace Models
                             --count;
                         }
                         #endregion
-
+#endif
                         data[j * resolution + i] = repert - count;
                     }
                 }
@@ -352,42 +255,45 @@ namespace Models
             return Task.Run<int[]>(() => {
                 int[] data = new int[resolution * resolution];
 
+#if ComplexClassDefine
                 #region Complex Code
-                //Complex minimum = new Complex(xpos - radius, ypos - radius);
-                //Complex maximum = new Complex(xpos + radius, ypos + radius);
-                //Complex julia = new Complex(ipos, jpos);
+                Complex minimum = new Complex(xpos - radius, ypos - radius);
+                Complex maximum = new Complex(xpos + radius, ypos + radius);
+                Complex julia = new Complex(ipos, jpos);
                 #endregion
-
+#else
                 #region No Complex Code
                 double xmin = xpos - radius;
                 double ymin = ypos + radius;
                 #endregion
-
+#endif
                 int i = 0;
                 for (; i < resolution; i++)
                 {
+#if ComplexClassDefine
                     #region Complex Code
-                    //double x = i * (maximum.Real - minimum.Real) / (resolution - 1);
+                    double x = i * (maximum.Real - minimum.Real) / (resolution - 1);
                     #endregion
-
+#else
                     #region No Complex Code
                     double x = i * radius * 2 / (resolution - 1) + xmin;
                     #endregion
-
+#endif
                     for (int j = 0; j < resolution; j++)
                     {
                         int count = repert;
 
+#if ComplexClassDefine
                         #region Complex Code
-                        //double y = j * (maximum.Imaginary - minimum.Imaginary) / (resolution - 1);
-                        //Complex z = new Complex(minimum.Real + x, maximum.Imaginary - y);
-                        //while (z.Magnitude < 2 && count > 0)
-                        //{
-                        //    z = (z * z) + julia;
-                        //    --count;
-                        //}
+                        double y = j * (maximum.Imaginary - minimum.Imaginary) / (resolution - 1);
+                        Complex z = new Complex(minimum.Real + x, maximum.Imaginary - y);
+                        while (z.Magnitude < 2 && count > 0)
+                        {
+                            z = (z * z) + julia;
+                            --count;
+                        }
                         #endregion
-
+#else
                         #region No Complex Code
                         double y = j * radius * 2 / (resolution - 1) - ymin;
                         double a = x;
@@ -403,7 +309,7 @@ namespace Models
                             --count;
                         }
                         #endregion
-
+#endif
                         data[j * resolution + i] = repert - count;
                     }
                 }
@@ -433,7 +339,6 @@ namespace Models
 
                 int[] data = new int[resolution * resolution];
 
-                //double radius = 2;
                 double julia = radius * 2 / (split - 1);
 
                 List<Task<int[]>> tasks = new List<Task<int[]>>();
