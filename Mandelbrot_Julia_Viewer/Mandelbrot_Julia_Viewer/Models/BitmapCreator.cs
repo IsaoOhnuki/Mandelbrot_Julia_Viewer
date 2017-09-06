6e013ea99d0ef9e6de984aa668beb9b1adf29db4
ISAO-SERVER\isao <isa0.net@ezweb.ne.jp>
2017-09-07 04:53:26 +0900

    BitmapCreator修正

--- Mandelbrot_Julia_Viewer/Mandelbrot_Julia_Viewer/Models/BitmapCreator.cs
+++ Mandelbrot_Julia_Viewer/Mandelbrot_Julia_Viewer/Models/BitmapCreator.cs
@@ -8,74 +8,81 @@ namespace Models
 {
     public class BitmapCreator
     {
-        public static Task<byte[]> Create(short Width, short Height, byte[] data)
+        public static Task<byte[]> Create(short Width, short Height, byte[] data, bool useAlpha)
         {
             return Task<byte[]>.Run(() => {
                 System.IO.MemoryStream bmp = new System.IO.MemoryStream();
                 System.IO.BinaryWriter bw = new System.IO.BinaryWriter(bmp);
 
-                // OS/2Bitmap
-                // bitmapヘッダ
-                int widthFiller = (Width * 3) % 4;
-                widthFiller = widthFiller == 0 ? 0 : 4 - widthFiller;
-                bw.Write((byte)'B');
-                bw.Write((byte)'M');
-                bw.Write((int)14 + 12 + (Width * 3 + widthFiller) * Height);
-                bw.Write((short)0);
-                bw.Write((short)0);
-                bw.Write((int)14 + 12);
-                // 情報ヘッダ
-                bw.Write((int)12);
-                bw.Write((short)Width);
-                bw.Write((short)Height);
-                bw.Write((short)1);
-                bw.Write((short)24);
-                // イメージブロック
-                for (int h = Height - 1; h >= 0; --h)
+                if (useAlpha)
                 {
-                    for (int w = 0; w < Width * 4; w += 4)
+                    // WindowsBitmap
+                    // bitmapヘッダ
+                    bw.Write((byte)'B');
+                    bw.Write((byte)'M');
+                    bw.Write((int)14 + 40 + Width * 4 * Height);
+                    bw.Write((short)0);
+                    bw.Write((short)0);
+                    bw.Write((int)14 + 40);
+                    // 情報ヘッダ
+                    bw.Write((int)40);
+                    bw.Write((int)Width);
+                    bw.Write((int)Height);
+                    bw.Write((short)1);
+                    bw.Write((short)32);
+                    bw.Write((int)0);
+                    bw.Write((int)Width * Height * 4);
+                    bw.Write((int)3780);
+                    bw.Write((int)3780);
+                    bw.Write((int)0);
+                    bw.Write((int)0);
+                    // イメージブロック
+                    for (int h = Height - 1; h >= 0; --h)
                     {
-                        bw.Write(data[h * Width * 4 + w]); // B値
-                        bw.Write(data[h * Width * 4 + w + 1]); // G値
-                        bw.Write(data[h * Width * 4 + w + 2]); // R値
-                        //bw.Write(data[h * Width * 4 + w + 3]); α値は捨てる
-                    }
-                    for (int w = 0; w < widthFiller; ++w)
-                    {
-                        bw.Write((byte)0); // 4バイト境界調整
+                        for (int w = 0; w < Width * 4; w += 4)
+                        {
+                            bw.Write(data[h * Width * 4 + w]); // B値
+                            bw.Write(data[h * Width * 4 + w + 1]); // G値
+                            bw.Write(data[h * Width * 4 + w + 2]); // R値
+                            bw.Write(data[h * Width * 4 + w + 3]); // α値
+                        }
                     }
                 }
-                //// WindowsBitmap
-                //// bitmapヘッダ
-                //bw.Write((byte)'B');
-                //bw.Write((byte)'M');
-                //bw.Write((int)14 + 40 + Width * 4 * Height);
-                //bw.Write((short)0);
-                //bw.Write((short)0);
-                //bw.Write((int)14 + 40);
-                //// 情報ヘッダ
-                //bw.Write((int)40);
-                //bw.Write((int)Width);
-                //bw.Write((int)Height);
-                //bw.Write((short)1);
-                //bw.Write((short)32);
-                //bw.Write((int)0);
-                //bw.Write((int)Width * Height * 4);
-                //bw.Write((int)3780);
-                //bw.Write((int)3780);
-                //bw.Write((int)0);
-                //bw.Write((int)0);
-                //// イメージブロック
-                //for (int h = Height - 1; h >= 0; --h)
-                //{
-                //    for (int w = 0; w < Width * 4; w += 4)
-                //    {
-                //        bw.Write(data[h * Width * 4 + w]); // B値
-                //        bw.Write(data[h * Width * 4 + w + 1]); // G値
-                //        bw.Write(data[h * Width * 4 + w + 2]); // R値
-                //        bw.Write(data[h * Width * 4 + w + 3]); // α値
-                //    }
-                //}
+                else
+                {
+                    // OS/2Bitmap
+                    // bitmapヘッダ
+                    int widthFiller = (Width * 3) % 4;
+                    widthFiller = widthFiller == 0 ? 0 : 4 - widthFiller;
+                    bw.Write((byte)'B');
+                    bw.Write((byte)'M');
+                    bw.Write((int)14 + 12 + (Width * 3 + widthFiller) * Height);
+                    bw.Write((short)0);
+                    bw.Write((short)0);
+                    bw.Write((int)14 + 12);
+                    // 情報ヘッダ
+                    bw.Write((int)12);
+                    bw.Write((short)Width);
+                    bw.Write((short)Height);
+                    bw.Write((short)1);
+                    bw.Write((short)24);
+                    // イメージブロック
+                    for (int h = Height - 1; h >= 0; --h)
+                    {
+                        for (int w = 0; w < Width * 4; w += 4)
+                        {
+                            bw.Write(data[h * Width * 4 + w]); // B値
+                            bw.Write(data[h * Width * 4 + w + 1]); // G値
+                            bw.Write(data[h * Width * 4 + w + 2]); // R値
+                                                                   //bw.Write(data[h * Width * 4 + w + 3]); α値は捨てる
+                        }
+                        for (int w = 0; w < widthFiller; ++w)
+                        {
+                            bw.Write((byte)0); // 4バイト境界調整
+                        }
+                    }
+                }
+
                 bw.Flush();
                 bw.Dispose();
                 return bmp.ToArray();
