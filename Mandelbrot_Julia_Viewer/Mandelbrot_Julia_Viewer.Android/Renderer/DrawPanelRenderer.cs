bb03af6f4214d635d235708491a68faac016f41b
ISAO-SERVER\isao <isa0.net@ezweb.ne.jp>
2017-09-09 03:00:38 +0900

    DrawPanel追加

---
+++ Mandelbrot_Julia_Viewer/Mandelbrot_Julia_Viewer.Android/Renderer/DrawPanelRenderer.cs
@@ -0,0 +1,27 @@
+﻿using Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Android.Views;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(DrawPanel), typeof(Mandelbrot_Julia_Viewer.Droid.DrawPanelRenderer))]
+namespace Mandelbrot_Julia_Viewer.Droid
+{
+    class DrawPanelRenderer : ViewRenderer<GradationDrawer, SurfaceView>
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<GradationDrawer> e)
+        {
+            base.OnElementChanged(e);
+        }
+
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
+        }
+    }
+}
---
+++ Mandelbrot_Julia_Viewer/Mandelbrot_Julia_Viewer.UWP/Renderer/DrawPanelRenderer.cs
@@ -0,0 +1,26 @@
+﻿using Controls;
+using Microsoft.Graphics.Canvas.UI.Xaml;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms.Platform.UWP;
+using System.ComponentModel;
+
+[assembly: ExportRenderer(typeof(DrawPanel), typeof(Mandelbrot_Julia_Viewer.UWP.DrawPanelRenderer))]
+namespace Mandelbrot_Julia_Viewer.UWP
+{
+    class DrawPanelRenderer : ViewRenderer<GradationDrawer, CanvasControl>
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<GradationDrawer> e)
+        {
+            base.OnElementChanged(e);
+        }
+
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
+        }
+    }
+}
