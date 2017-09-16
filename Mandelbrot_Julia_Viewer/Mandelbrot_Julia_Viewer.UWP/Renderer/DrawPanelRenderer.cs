e64c3383e5be69946287d43d61f664c99a01a27c
KAIKAN-6\kaikan-6 <isa0.net@ezweb.ne.jp>
2017-09-16 15:59:07 +0900

    uwp DrawPanelRendererジェスチャー実装

--- Mandelbrot_Julia_Viewer/Mandelbrot_Julia_Viewer.UWP/Renderer/DrawPanelRenderer.cs
+++ Mandelbrot_Julia_Viewer/Mandelbrot_Julia_Viewer.UWP/Renderer/DrawPanelRenderer.cs
@@ -13,6 +13,11 @@ using Windows.UI.Xaml.Media.Imaging;
 using Windows.Storage.Streams;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Xamarin.Forms;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Input;
+using Windows.UI.Xaml.Controls;
 
 [assembly: ExportRenderer(typeof(DrawPanel), typeof(Mandelbrot_Julia_Viewer.UWP.DrawPanelRenderer))]
 namespace Mandelbrot_Julia_Viewer.UWP
@@ -44,40 +49,18 @@ namespace Mandelbrot_Julia_Viewer.UWP
             }
         }
 
-        public async Task<bool> UpdateImageASync()
-        {
-            if (Element.ImageDataValid)
-            {
-                DrawPanel.DrawImage image = await Task<DrawPanel.DrawImage>.Factory.StartNew(sz => Element.GetDrawImage(Origin, Scale, (Size)sz, false), new Size(Control.ActualWidth, Control.ActualHeight));
-                //Scale = image.Scale;
-                Origin = image.Origin;
-                Image = null;
-                try
-                {
-                    var dvc = CanvasDevice.GetSharedDevice();
-                    Image = await Task.Run<CanvasBitmap>(() => CanvasBitmap.CreateFromBytes(dvc, image.Image, image.ImageSizeX, image.ImageSizeY, Windows.Graphics.DirectX.DirectXPixelFormat.B8G8R8A8UIntNormalized, 96));
-                    DrawRect = new Windows.Foundation.Rect(image.DrawRect.Location.X, image.DrawRect.Location.Y, image.DrawRect.Size.Width, image.DrawRect.Size.Height);
-                }
-                catch
-                {
-                    DrawRect = new Windows.Foundation.Rect(0, 0, 0, 0);
-                    return false;
-                }
-                return true;
-            }
-            return false;
-        }
-
         public bool UpdateImage()
         {
             if (Element.ImageDataValid)
             {
-                DrawPanel.DrawImage image = Element.GetDrawImage(Origin, Scale, new Size(Control.ActualWidth, Control.ActualHeight), false);
-                //Scale = image.Scale;
-                Origin = image.Origin;
-                Image = null;
-                Image = CanvasBitmap.CreateFromBytes(Control, image.Image, image.ImageSizeX, image.ImageSizeY, Windows.Graphics.DirectX.DirectXPixelFormat.B8G8R8A8UIntNormalized, 96);
-                DrawRect = new Windows.Foundation.Rect(image.DrawRect.Location.X, image.DrawRect.Location.Y, image.DrawRect.Size.Width, image.DrawRect.Size.Height);
+                double drawWidth = Element.ImageWidth * Scale;
+                double drawHeight = Element.ImageHeight * Scale;
+
+                DrawRect = new Windows.Foundation.Rect(
+                    Origin.X - drawWidth / 2 + Control.ActualWidth / 2,
+                    Origin.Y - drawHeight / 2 + Control.ActualHeight / 2,
+                    drawWidth,
+                    drawHeight);
                 return true;
             }
             return false;
@@ -105,8 +88,14 @@ namespace Mandelbrot_Julia_Viewer.UWP
                 Control.Tapped += Control_Tapped;
                 Control.ManipulationDelta += Control_ManipulationDelta;
                 Control.PointerWheelChanged += Control_PointerWheelChanged;
+                Control.DragStarting += Control_DragStarting;
             }
             base.OnElementChanged(e);
+        }
+
+        private void Control_DragStarting(Windows.UI.Xaml.UIElement sender, Windows.UI.Xaml.DragStartingEventArgs args)
+        {
+            throw new NotImplementedException();
         }
 
         protected override async void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
@@ -118,20 +107,22 @@ namespace Mandelbrot_Julia_Viewer.UWP
                 || e.PropertyName == DrawPanel.ImageWidthProperty.PropertyName
                 || e.PropertyName == DrawPanel.ImageHeightProperty.PropertyName)
             {
-                if (await UpdateImageASync())
-                //if (UpdateImage())
+                Image = await Task<CanvasBitmap>.Run(() => {
+                    var dvc = CanvasDevice.GetSharedDevice();
+                    return CanvasBitmap.CreateFromBytes(dvc, Element.ImageData, Element.ImageWidth, Element.ImageHeight, Windows.Graphics.DirectX.DirectXPixelFormat.B8G8R8A8UIntNormalized, 96);
+                });
+                if (UpdateImage())
                     Control.Invalidate();
             }
         }
 
-        private async void Control_SizeChanged(object sender, Windows.UI.Xaml.SizeChangedEventArgs e)
+        private void Control_SizeChanged(object sender, Windows.UI.Xaml.SizeChangedEventArgs e)
         {
-            if (await UpdateImageASync())
-            //if (UpdateImage())
+            if (UpdateImage())
                 Control.Invalidate();
         }
 
-        private async void Control_PointerWheelChanged(object sender, Windows.UI.Xaml.Input.PointerRoutedEventArgs e)
+        private void Control_PointerWheelChanged(object sender, Windows.UI.Xaml.Input.PointerRoutedEventArgs e)
         {
             int delta = e.GetCurrentPoint(Control).Properties.MouseWheelDelta;
             double oldScale = Scale;
@@ -143,16 +134,14 @@ namespace Mandelbrot_Julia_Viewer.UWP
             {
                 Scale += 0.2 * delta / 120 * Scale;
             }
-            if (await UpdateImageASync())
-            //if (UpdateImage())
+            if (UpdateImage())
                 Control.Invalidate();
         }
 
-        private async void Control_ManipulationDelta(object sender, Windows.UI.Xaml.Input.ManipulationDeltaRoutedEventArgs e)
+        private void Control_ManipulationDelta(object sender, Windows.UI.Xaml.Input.ManipulationDeltaRoutedEventArgs e)
         {
             Scale *= e.Delta.Scale;
-            if (await UpdateImageASync())
-            //if (UpdateImage())
+            if (UpdateImage())
                 Control.Invalidate();
         }
 
@@ -167,6 +156,265 @@ namespace Mandelbrot_Julia_Viewer.UWP
             {
                 args.DrawingSession.DrawImage(Image, DrawRect);
             }
+        }
+    }
+
+
+    // https://github.com/Microsoft/Windows-universal-samples/tree/master/Samples/BasicInput Basic input sample
+    public sealed partial class Scenario5 : Windows.UI.Xaml.Controls.Page
+    {
+        Windows.UI.Input.GestureRecognizer recognizer;
+        ManipulationInputProcessor manipulationProcessor;
+
+        public Scenario5()
+        {
+            //this.InitializeComponent();
+
+            InitOptions();
+
+            // Create a GestureRecognizer which will be used to process the manipulations
+            // done on the rectangle
+            recognizer = new Windows.UI.Input.GestureRecognizer();
+
+            // Create a ManipulationInputProcessor which will listen for events on the
+            // rectangle, process them, and update the rectangle's position, size, and rotation
+            ////manipulationProcessor = new ManipulationInputProcessor(recognizer, manipulateMe, mainCanvas);
+            //< Canvas x: Name = "mainCanvas" ManipulationMode = "None" Margin = "0,12,0,0" MinHeight = "400" >
+            //    < Border x: Name = "manipulateMe" Background = "LightGray" Height = "200" Width = "200"  ManipulationMode = "All" />
+            //</ Canvas >
+        }
+
+        private void InitOptions()
+        {
+            ////movementAxis.SelectedIndex = 0;
+            ////InertiaSwitch.IsOn = true;
+            //< ComboBox Name = "movementAxis" Header = "Movement Axis" SelectionChanged = "movementAxis_Changed" VerticalAlignment = "Center" Margin = "0,12,0,0" >
+            //    < ComboBoxItem Content = "X and Y" />
+            //    < ComboBoxItem Content = "X only" />
+            //    < ComboBoxItem Content = "Y only" />
+            //</ ComboBox >
+            //< ToggleSwitch x: Name = "InertiaSwitch" Header = "Inertia" Margin = "0,12,0,0" Toggled = "InertiaSwitch_Toggled" />
+        }
+
+        private void movementAxis_Changed(object sender, SelectionChangedEventArgs e)
+        {
+            if (manipulationProcessor == null)
+            {
+                return;
+            }
+
+            ComboBoxItem selectedItem = (ComboBoxItem)((ComboBox)sender).SelectedItem;
+            switch (selectedItem.Content.ToString())
+            {
+                case "X only":
+                    manipulationProcessor.LockToXAxis();
+                    break;
+                case "Y only":
+                    manipulationProcessor.LockToYAxis();
+                    break;
+                default:
+                    manipulationProcessor.MoveOnXAndYAxes();
+                    break;
+            }
+        }
+
+        private void InertiaSwitch_Toggled(object sender, RoutedEventArgs e)
+        {
+            if (manipulationProcessor == null)
+            {
+                return;
+            }
+
+            ////manipulationProcessor.UseInertia(InertiaSwitch.IsOn);
+        }
+
+        void resetButton_Pressed(object sender, RoutedEventArgs e)
+        {
+            InitOptions();
+            manipulationProcessor.Reset();
+        }
+    }
+
+    class ManipulationInputProcessor
+    {
+        Windows.UI.Input.GestureRecognizer recognizer;
+        Windows.UI.Xaml.UIElement element;
+        Windows.UI.Xaml.UIElement reference;
+        TransformGroup cumulativeTransform;
+        MatrixTransform previousTransform;
+        CompositeTransform deltaTransform;
+
+        public ManipulationInputProcessor(Windows.UI.Input.GestureRecognizer gestureRecognizer, UIElement target, UIElement referenceFrame)
+        {
+            recognizer = gestureRecognizer;
+            element = target;
+            reference = referenceFrame;
+
+            // Initialize the transforms that will be used to manipulate the shape
+            InitializeTransforms();
+
+            // The GestureSettings property dictates what manipulation events the
+            // Gesture Recognizer will listen to.  This will set it to a limited
+            // subset of these events.
+            recognizer.GestureSettings = GenerateDefaultSettings();
+
+            // Set up pointer event handlers. These receive input events that are used by the gesture recognizer.
+            element.PointerPressed += OnPointerPressed;
+            element.PointerMoved += OnPointerMoved;
+            element.PointerReleased += OnPointerReleased;
+            element.PointerCanceled += OnPointerCanceled;
+
+            // Set up event handlers to respond to gesture recognizer output
+            recognizer.ManipulationStarted += OnManipulationStarted;
+            recognizer.ManipulationUpdated += OnManipulationUpdated;
+            recognizer.ManipulationCompleted += OnManipulationCompleted;
+            recognizer.ManipulationInertiaStarting += OnManipulationInertiaStarting;
+        }
+
+        public void InitializeTransforms()
+        {
+            cumulativeTransform = new TransformGroup();
+            deltaTransform = new CompositeTransform();
+            previousTransform = new MatrixTransform() { Matrix = Matrix.Identity };
+
+            cumulativeTransform.Children.Add(previousTransform);
+            cumulativeTransform.Children.Add(deltaTransform);
+
+            element.RenderTransform = cumulativeTransform;
+        }
+
+        // Return the default GestureSettings for this sample
+        GestureSettings GenerateDefaultSettings()
+        {
+            return GestureSettings.ManipulationTranslateX |
+                GestureSettings.ManipulationTranslateY |
+                GestureSettings.ManipulationRotate |
+                GestureSettings.ManipulationTranslateInertia |
+                GestureSettings.ManipulationRotateInertia;
+        }
+
+        // Route the pointer pressed event to the gesture recognizer.
+        // The points are in the reference frame of the canvas that contains the rectangle element.
+        void OnPointerPressed(object sender, PointerRoutedEventArgs args)
+        {
+            // Set the pointer capture to the element being interacted with so that only it
+            // will fire pointer-related events
+            element.CapturePointer(args.Pointer);
+
+            // Feed the current point into the gesture recognizer as a down event
+            recognizer.ProcessDownEvent(args.GetCurrentPoint(reference));
+        }
+
+        // Route the pointer moved event to the gesture recognizer.
+        // The points are in the reference frame of the canvas that contains the rectangle element.
+        void OnPointerMoved(object sender, PointerRoutedEventArgs args)
+        {
+            // Feed the set of points into the gesture recognizer as a move event
+            recognizer.ProcessMoveEvents(args.GetIntermediatePoints(reference));
+        }
+
+        // Route the pointer released event to the gesture recognizer.
+        // The points are in the reference frame of the canvas that contains the rectangle element.
+        void OnPointerReleased(object sender, PointerRoutedEventArgs args)
+        {
+            // Feed the current point into the gesture recognizer as an up event
+            recognizer.ProcessUpEvent(args.GetCurrentPoint(reference));
+
+            // Release the pointer
+            element.ReleasePointerCapture(args.Pointer);
+        }
+
+        // Route the pointer canceled event to the gesture recognizer.
+        // The points are in the reference frame of the canvas that contains the rectangle element.
+        void OnPointerCanceled(object sender, PointerRoutedEventArgs args)
+        {
+            recognizer.CompleteGesture();
+            element.ReleasePointerCapture(args.Pointer);
+        }
+
+        // When a manipulation begins, change the color of the object to reflect
+        // that a manipulation is in progress
+        void OnManipulationStarted(object sender, ManipulationStartedEventArgs e)
+        {
+            Border b = element as Border;
+            b.Background = new SolidColorBrush(Windows.UI.Colors.DeepSkyBlue);
+        }
+
+        // Process the change resulting from a manipulation
+        void OnManipulationUpdated(object sender, ManipulationUpdatedEventArgs e)
+        {
+            previousTransform.Matrix = cumulativeTransform.Value;
+
+            // Get the center point of the manipulation for rotation
+            Point center = new Point(e.Position.X, e.Position.Y);
+            deltaTransform.CenterX = center.X;
+            deltaTransform.CenterY = center.Y;
+
+            // Look at the Delta property of the ManipulationDeltaRoutedEventArgs to retrieve
+            // the rotation, X, and Y changes
+            deltaTransform.Rotation = e.Delta.Rotation;
+            deltaTransform.TranslateX = e.Delta.Translation.X;
+            deltaTransform.TranslateY = e.Delta.Translation.Y;
+        }
+
+        // When a manipulation that's a result of inertia begins, change the color of the
+        // the object to reflect that inertia has taken over
+        void OnManipulationInertiaStarting(object sender, ManipulationInertiaStartingEventArgs e)
+        {
+            Border b = element as Border;
+            b.Background = new SolidColorBrush(Windows.UI.Colors.RoyalBlue);
+        }
+
+        // When a manipulation has finished, reset the color of the object
+        void OnManipulationCompleted(object sender, ManipulationCompletedEventArgs e)
+        {
+            Border b = element as Border;
+            b.Background = new SolidColorBrush(Windows.UI.Colors.LightGray);
+        }
+
+        // Modify the GestureSettings property to only allow movement on the X axis
+        public void LockToXAxis()
+        {
+            recognizer.CompleteGesture();
+            recognizer.GestureSettings |= GestureSettings.ManipulationTranslateY | GestureSettings.ManipulationTranslateX;
+            recognizer.GestureSettings ^= GestureSettings.ManipulationTranslateY;
+        }
+
+        // Modify the GestureSettings property to only allow movement on the Y axis
+        public void LockToYAxis()
+        {
+            recognizer.CompleteGesture();
+            recognizer.GestureSettings |= GestureSettings.ManipulationTranslateY | GestureSettings.ManipulationTranslateX;
+            recognizer.GestureSettings ^= GestureSettings.ManipulationTranslateX;
+        }
+
+        // Modify the GestureSettings property to allow movement on both the the X and Y axes
+        public void MoveOnXAndYAxes()
+        {
+            recognizer.CompleteGesture();
+            recognizer.GestureSettings |= GestureSettings.ManipulationTranslateX | GestureSettings.ManipulationTranslateY;
+        }
+
+        // Modify the GestureSettings property to enable or disable inertia based on the passed-in value
+        public void UseInertia(bool inertia)
+        {
+            if (!inertia)
+            {
+                recognizer.CompleteGesture();
+                recognizer.GestureSettings ^= GestureSettings.ManipulationTranslateInertia | GestureSettings.ManipulationRotateInertia;
+            }
+            else
+            {
+                recognizer.GestureSettings |= GestureSettings.ManipulationTranslateInertia | GestureSettings.ManipulationRotateInertia;
+            }
+        }
+
+        public void Reset()
+        {
+            element.RenderTransform = null;
+            recognizer.CompleteGesture();
+            InitializeTransforms();
+            recognizer.GestureSettings = GenerateDefaultSettings();
         }
     }
 }
