28f505283f41048b9ac5d2fd1fe84817e2b799bb
IsaoOhnuki <isa0_net@hotmail.com>
2020-08-11 13:14:52 +0900

    デスクトッププロジェクト作成

---
+++ Mandelbrot_Julia_Viewer.Desktop/Models/MainWindowModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Mandelbrot_Julia_Viewer.Desktop.Models
+{
+    public class BindableBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string propertyName)
+        {
+            OnPropertyChanged(new PropertyChangedEventArgs(propertyName));
+        }
+
+        public void OnPropertyChanged(PropertyChangedEventArgs args)
+        {
+            PropertyChanged?.Invoke(this, args);
+        }
+        public void SetProperty<T>(ref T storage, T value, string propertyName)
+        {
+            storage = value;
+            OnPropertyChanged(propertyName);
+        }
+    }
+
+    public class MainWindowModel : BindableBase
+    {
+        MainWindowModel _instance;
+        public MainWindowModel Instance => _instance ?? (_instance = new MainWindowModel());
+
+        public MainWindowModel()
+        {
+        }
+    }
+}
---
+++ Mandelbrot_Julia_Viewer.Desktop/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Mandelbrot_Julia_Viewer.Desktop.ViewModels
+{
+    public class CommandHandler : ICommand
+    {
+        private readonly Func<object, bool> _CanExecute;
+        private readonly Action<object> _Execute;
+
+        public CommandHandler(Action<object> Execute, Func<object, bool> CanExecute = null)
+        {
+            _Execute = Execute;
+            _CanExecute = CanExecute;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            if (_CanExecute == null)
+            {
+                return true;
+            }
+            return _CanExecute.Invoke(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            _Execute?.Invoke(parameter);
+        }
+
+        public void DoCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+
+    public class MainWindowViewModel : DependencyObject
+    {
+    }
+}
